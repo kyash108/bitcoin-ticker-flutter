9cae6f221c931269ed03e58dcb9ff10f2c8af232
Yash Kumar <w0749495@myscc.ca>
2022-08-24 22:08:30 -0400

    added the loop to go through the list of currency for the drop down currency item

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  List<DropdownMenuItem> getDropDownItems(){
+    List<DropdownMenuItem<String>> dropDownItem =[];
+    for(var i =0; i<currenciesList.length; i++){
+      var newItem = DropdownMenuItem(child: Text(currenciesList[i].toString()),
+        value: currenciesList[i].toString(),
+      );
+      dropDownItem.add(newItem);
+    };
+    return dropDownItem;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +56,16 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: getDropDownItems(),
+              onChanged: (value){
+                setState(() {
+                  selectedCurrency = value;
+                });
+              },
+
+            ),
           ),
         ],
       ),
